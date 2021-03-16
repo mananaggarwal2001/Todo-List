d07d45bf997c8da360b0103ba0225f52605f0f7b
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-03-16 11:43:24 +0530

    app commit

--- app.js
+++ app.js
@@ -6,34 +6,41 @@ const app = express();
 
 console.log(date());
 
-var items = ["buy Food ","Cook Food","Eat Food"];
+var items = ["buy Food ", "Cook Food", "Eat Food"];
 var workListItems = [];
 
-var item="";
+var item = "";
 let workItem = "";
 
 app.set('view engine', 'ejs');
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-let day = date();
+    let day = date();
 
-res.render('list', {
-    listTitle: day,
-    newListItems:items,
-  });
+    res.render('list', {
+        listTitle: day,
+        newListItems: items,
+    });
 })
 
-app.post("/" , function(req , res){
-  item = req.body.todoListName;
-    items.push(item);
-    res.redirect("/");
+app.post("/", function(req, res) {
+    item = req.body.todoListName;
+    workItem = req.body.todoListName;
 })
 
-app.get("/about",function(req , res){
-  res.render('about');
+app.get("/workitem", (req, res) => {
+
+    let day = date();
+
+    res.render('list', {
+        listTitle: day,
+        newListItems: workListItems
+    });
 })
+
+
 app.listen(3000, function() {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 })
