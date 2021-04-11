521800a44ab3875eb852031a85c1f095e1d3d261
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-04-12 00:30:28 +0530

    Custom list

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ const newItemTwo = new itemModel({
 
 const defaultItemList = [newItem, newItemOne, newItemTwo];
 
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+}
+
+const listModel = mongoose.model("List", listSchema); // for storing the list heading in the List collections in the database of todolistDB.
+
 app.get("/", function(req, res) {
     let day = date();
     itemModel.find({}, (err, results) => {
@@ -54,10 +62,6 @@ app.get("/", function(req, res) {
     })
 })
 
-
-
-
-
 app.post("/deleteItem", (req, res) => {
     const checkedItemId = req.body.checkedItem;
     itemModel.findByIdAndRemove({ _id: checkedItemId }, (err) => {
@@ -70,7 +74,6 @@ app.post("/deleteItem", (req, res) => {
 
     res.redirect("/");
 })
-
 app.post("/", function(req, res) {
     const itemName = req.body.todoListName;
     const insertItemname = new itemModel({
@@ -79,10 +82,30 @@ app.post("/", function(req, res) {
     insertItemname.save();
     res.redirect("/");
 })
+app.get("/:customListName", (req, res) => {
+    let parameters = req.params.customListName;
 
-// app.get("/workitem", (req, res) => {
-//     res.render("list", { listTitle: "Work" })
-// })
+    listModel.findOne({ name: parameters }, (err, results) => {
+        if (!err) {
+            if (!results) {
+                const List = new listModel({
+                    name: parameters,
+                    items: defaultItemList
+                });
+
+                List.save(); // this is for the new list which is made .
+
+                res.redirect("/" + parameters);
+            } else {
+                res.render('list', {
+                    listTitle: results.name,
+                    newListItems: results.items, // this is for the existing list which is found
+                });
+
+            }
+        }
+    })
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
